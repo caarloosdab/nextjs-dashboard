dbd6e113ece1a457a8039ce5b63aa36fef0123d7
caarloosdab <caarloosdab@gmail.com>
2025-11-17 23:08:04 -0600

    `Update metadata titles for various pages`

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,21 @@
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function CustomersPage(props: {
+  searchParams?: Promise<{ query?: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <CustomersTable customers={customers} />
+    </div>
+  );
+}
